1e8f72efdf913fba43d3dc1ca934079f198cdd7a
lilijo6 <64238731+lilijo6@users.noreply.github.com>
2020-09-17 01:25:35 -0400

    deleted statelogic2, dummyjson, statedata.js, linked html pages, trying to fix renewables and state

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,28 +1,29 @@
 // Creating layers for map
 // Adding tile layer
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 4,
-  minZoom: 4,
-  zoomOffset: -1,
-  id: "mapbox/light-v10",
-  accessToken: API_KEY
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 4,
+    minZoom: 4,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
 });
 
 //Initialize all of the LayerGroups
 var layers = {
-  CONSUMPTION : new L.LayerGroup(),
-  PRODUCTION : new L.LayerGroup(),
-  EMISSIONS : new L.LayerGroup()
+    CONSUMPTION: new L.LayerGroup(),
+    PRODUCTION: new L.LayerGroup(),
+    EMISSIONS: new L.LayerGroup()
 };
 
 // Define a map object
 var myMap = L.map("map", {
-  center: [40, -95],
-  zoom: 2,
-  layers: [
-    layers.PRODUCTION]
+    center: [40, -95],
+    zoom: 2,
+    layers: [
+        layers.PRODUCTION
+    ]
 });
 
 lightmap.addTo(myMap);
@@ -36,24 +37,24 @@ var overlays = {
 
 // Pass our map layers into our layer control
 // Add the layer control to the map
-L.control.layers(overlays, null, {collapsed:false}).addTo(myMap);
+L.control.layers(overlays, null, { collapsed: false }).addTo(myMap);
 
 var path = "data/states.geojson";
 
 d3.json(path, function(err, data) {
-  if(err) console.log("error fetching data");
-  console.log(data);
+    if (err) console.log("error fetching data");
+    console.log(data);
 
-    function statedata(statename){
-      //console.log("Inside - On click method")
-      var currenturl=window.location.href;
-      console.log(currenturl)
-      var newurl=currenturl+`state.html?name=${statename}`
-      window.location.href = newurl;
+    function statedata(statename) {
+        //console.log("Inside - On click method")
+        var currenturl = window.location.href.split('/')[2];;
+        console.log(currenturl)
+        var newurl = currenturl + `state.html?name=${statename}`
+        window.location.href = newurl;
     }
 
-      // State Energy Production layer
-      var geojsonP = L.choropleth(data, {
+    // State Energy Production layer
+    var geojsonP = L.choropleth(data, {
 
         // Define what property in the features to use
         valueProperty: "ProductionShare",
@@ -63,42 +64,42 @@ d3.json(path, function(err, data) {
         steps: 8,
         mode: "q",
         style: {
-          // Border color
-          color: "#fff",
-          weight: 1,
-          fillOpacity: 0.8
+            // Border color
+            color: "#fff",
+            weight: 1,
+            fillOpacity: 0.8
         },
 
         onEachFeature: function(feature, layer) {
 
-          // Set mouse events to change map styling on mouseover and mouseout
-          layer.on({
-            mouseover: function(event) {
-              layer = event.target;
-              layer.setStyle({
-                fillOpacity: 1
-              });
-            },
-            mouseout: function(event) {
-              layer = event.target;
-              layer.setStyle({
-                fillOpacity: 0.8
-              });
-            },
-            click:function(event){
-              console.log(event.target.feature.properties.StateAbbreviation)
-              statedata(event.target.feature.properties.StateAbbreviation)
-            }
-          });
+            // Set mouse events to change map styling on mouseover and mouseout
+            layer.on({
+                mouseover: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                        fillOpacity: 1
+                    });
+                },
+                mouseout: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                        fillOpacity: 0.8
+                    });
+                },
+                click: function(event) {
+                    console.log(event.target.feature.properties.StateAbbreviation)
+                    statedata(event.target.feature.properties.StateAbbreviation)
+                }
+            });
 
-          // Binding a pop-up to each layer
-          layer.bindTooltip(feature.properties.NAME + "<br>" + feature.properties.ProductionShare + " trillion BTU" + "<br>" + 
-            "Rank: " + feature.properties.ProductionRank);
-          }
+            // Binding a pop-up to each layer
+            layer.bindTooltip(feature.properties.NAME + "<br>" + feature.properties.ProductionShare + " trillion BTU" + "<br>" +
+                "Rank: " + feature.properties.ProductionRank);
+        }
 
-      }).addTo(layers.PRODUCTION);
-    
-      var geojsonC = L.choropleth(data, {
+    }).addTo(layers.PRODUCTION);
+
+    var geojsonC = L.choropleth(data, {
 
         // Define what property in the features to use
         valueProperty: "ConsumptionShare",
@@ -108,43 +109,43 @@ d3.json(path, function(err, data) {
         steps: 8,
         mode: "q",
         style: {
-          // Border color
-          color: "#fff",
-          weight: 1,
-          fillOpacity: 0.8
+            // Border color
+            color: "#fff",
+            weight: 1,
+            fillOpacity: 0.8
         },
 
         onEachFeature: function(feature, layer) {
 
-          // Set mouse events to change map styling on mouseover and mouseout
-          layer.on({
-            mouseover: function(event) {
-              layer = event.target;
-              layer.setStyle({
-                fillOpacity: 1
-              });
-            },
-            mouseout: function(event) {
-              layer = event.target;
-              layer.setStyle({
-                fillOpacity: 0.8
-              });
-            },
-            click:function(event){
-              console.log(event.target.feature.properties.StateAbbreviation)
-              statedata(event.target.feature.properties.StateAbbreviation)
-            }
-          });
+            // Set mouse events to change map styling on mouseover and mouseout
+            layer.on({
+                mouseover: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                        fillOpacity: 1
+                    });
+                },
+                mouseout: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                        fillOpacity: 0.8
+                    });
+                },
+                click: function(event) {
+                    console.log(event.target.feature.properties.StateAbbreviation)
+                    statedata(event.target.feature.properties.StateAbbreviation)
+                }
+            });
 
-          // Binding a pop-up to each layer
-          layer.bindTooltip(feature.properties.NAME + "<br>" + feature.properties.ConsumptionShare + " million BTU" + "<br>" + 
-            "Rank: " + feature.properties.ConsumptionRank);
-          }
+            // Binding a pop-up to each layer
+            layer.bindTooltip(feature.properties.NAME + "<br>" + feature.properties.ConsumptionShare + " million BTU" + "<br>" +
+                "Rank: " + feature.properties.ConsumptionRank);
+        }
 
-      }).addTo(layers.CONSUMPTION);
+    }).addTo(layers.CONSUMPTION);
 
-      // State Energy Emissions layer
-      var geojsonE = L.choropleth(data, {
+    // State Energy Emissions layer
+    var geojsonE = L.choropleth(data, {
 
         // Define what property in the features to use
         valueProperty: "CarbonDioxideShare",
@@ -154,131 +155,130 @@ d3.json(path, function(err, data) {
         steps: 8,
         mode: "q",
         style: {
-          // Border color
-          color: "#fff",
-          weight: 1,
-          fillOpacity: 0.8
+            // Border color
+            color: "#fff",
+            weight: 1,
+            fillOpacity: 0.8
         },
 
         onEachFeature: function(feature, layer) {
 
-          // Set mouse events to change map styling on mouseover and mouseout
-          layer.on({
-            mouseover: function(event) {
-              layer = event.target;
-              layer.setStyle({
-                fillOpacity: 1
-              });
-            },
-            mouseout: function(event) {
-              layer = event.target;
-              layer.setStyle({
-                fillOpacity: 0.8
-              });
-            },
-            click:function(event){
-              console.log(event.target.feature.properties.StateAbbreviation)
-              statedata(event.target.feature.properties.StateAbbreviation)
-            }
-          });
+            // Set mouse events to change map styling on mouseover and mouseout
+            layer.on({
+                mouseover: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                        fillOpacity: 1
+                    });
+                },
+                mouseout: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                        fillOpacity: 0.8
+                    });
+                },
+                click: function(event) {
+                    console.log(event.target.feature.properties.StateAbbreviation)
+                    statedata(event.target.feature.properties.StateAbbreviation)
+                }
+            });
 
-          // Binding a pop-up to each layer
-          layer.bindTooltip(feature.properties.NAME + "<br>" + feature.properties.CarbonDioxideShare + " million metric tons" + "<br>" + 
-            "Rank: " + feature.properties.CarbonDioxideRank);
-          }
+            // Binding a pop-up to each layer
+            layer.bindTooltip(feature.properties.NAME + "<br>" + feature.properties.CarbonDioxideShare + " million metric tons" + "<br>" +
+                "Rank: " + feature.properties.CarbonDioxideRank);
+        }
 
-      }).addTo(layers.EMISSIONS);
+    }).addTo(layers.EMISSIONS);
 
-        // Set up the legend for total energy production
-        var legendP = L.control({ position: "bottomleft"});
-        legendP.onAdd = function() {
-          var div = L.DomUtil.create("div", "info legend");
-          var limits = geojsonP.options.limits;
-          var colors = geojsonP.options.colors;
-          var labels = [];
+    // Set up the legend for total energy production
+    var legendP = L.control({ position: "bottomleft" });
+    legendP.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+        var limits = geojsonP.options.limits;
+        var colors = geojsonP.options.colors;
+        var labels = [];
 
-          // Add min & max
-          var legendInfo = "<p>Consumption per Capita (million BTU)</p>" +
+        // Add min & max
+        var legendInfo = "<p>Consumption per Capita (million BTU)</p>" +
             "<div class=\"labels\">" +
             "<div class=\"min\">" + limits[0] + "</div>" +
             "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
             "</div>";
 
-          div.innerHTML = legendInfo;
+        div.innerHTML = legendInfo;
 
-          limits.forEach(function(limit, index) {
+        limits.forEach(function(limit, index) {
             labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-          });
+        });
 
-          div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-          return div;
-        };
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
 
-        legendP.addTo(layers.PRODUCTION);
+    legendP.addTo(layers.PRODUCTION);
 
-        // Set up the legend for energy consumption
-        var legendC = L.control({ position: "bottomleft"});
-        legendC.onAdd = function() {
-          var div = L.DomUtil.create("div", "info legend");
-          var limits = geojsonC.options.limits;
-          var colors = geojsonC.options.colors;
-          var labels = [];
+    // Set up the legend for energy consumption
+    var legendC = L.control({ position: "bottomleft" });
+    legendC.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+        var limits = geojsonC.options.limits;
+        var colors = geojsonC.options.colors;
+        var labels = [];
 
-          // Add min & max
-          var legendInfo = "<p>Total Energy Production (trillion BTU)</p>" +
+        // Add min & max
+        var legendInfo = "<p>Total Energy Production (trillion BTU)</p>" +
             "<div class=\"labels\">" +
             "<div class=\"min\">" + limits[0] + "</div>" +
             "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
             "</div>";
 
-          div.innerHTML = legendInfo;
+        div.innerHTML = legendInfo;
 
-          limits.forEach(function(limit, index) {
+        limits.forEach(function(limit, index) {
             labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-          });
+        });
 
-          div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-          return div;
-        };
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
 
-        legendC.addTo(layers.CONSUMPTION);
+    legendC.addTo(layers.CONSUMPTION);
 
-        // Set up the legend for total energy production
-        var legendE = L.control({ position: "bottomleft"});
-        legendE.onAdd = function() {
-          var div = L.DomUtil.create("div", "info legend");
-          var limits = geojsonE.options.limits;
-          var colors = geojsonE.options.colors;
-          var labels = [];
+    // Set up the legend for total energy production
+    var legendE = L.control({ position: "bottomleft" });
+    legendE.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+        var limits = geojsonE.options.limits;
+        var colors = geojsonE.options.colors;
+        var labels = [];
 
-          // Add min & max
-          var legendInfo = "<p>CO2 Emissions (million metric tons)</p>" +
+        // Add min & max
+        var legendInfo = "<p>CO2 Emissions (million metric tons)</p>" +
             "<div class=\"labels\">" +
             "<div class=\"min\">" + limits[0] + "</div>" +
             "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
             "</div>";
 
-          div.innerHTML = legendInfo;
+        div.innerHTML = legendInfo;
 
-          limits.forEach(function(limit, index) {
+        limits.forEach(function(limit, index) {
             labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-          });
+        });
 
-          div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-          return div;
-        };
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
 
-        // map.on('baselayerchange', function (eventLayer) {
-        //   // Switch to the Permafrost legend...
-        //      if (eventLayer.name === 'PRODUCTION') {
-        //          this.removeControl(legend);
-        //          legendP.addTo(this);}
-        //     else { // Or switch to the treeline legend...
-        //          this.removeControl(legendP);
-        //          legend.addTo(this);
-        //     }});
+    // map.on('baselayerchange', function (eventLayer) {
+    //   // Switch to the Permafrost legend...
+    //      if (eventLayer.name === 'PRODUCTION') {
+    //          this.removeControl(legend);
+    //          legendP.addTo(this);}
+    //     else { // Or switch to the treeline legend...
+    //          this.removeControl(legendP);
+    //          legend.addTo(this);
+    //     }});
 
-        legendE.addTo(layers.EMISSIONS);
+    legendE.addTo(layers.EMISSIONS);
 
 });
-
--- static/js/statelogic.js
+++ static/js/statelogic.js
@@ -1,94 +1,99 @@
-var StateEnergySourceURL = "api/'State_Energy_Production_Source_2018'";
-var StateProdConsURL = "api/State_EnergyData_2018";
+//console.log("In state data")
+//console.log(window.location.search) 
+const params = new URLSearchParams(window.location.search)
+const satename = params.get('name');
+console.log(satename);
+
+var StateEnergySourceURL = `http://localhost:5000/state/production?name=${satename}`;
+var StateProdConsURL = `http://localhost:5000/state/ranking?name=${satename}`;
 
 
 // create a function for the state info table
-function stateSummary(id) {
+function stateSummary() {
+    console.log("hello1");
     // read the data
-    d3.json(StateProdConsURL, StatePCdata => {
-        console.log(data);
-        var allStates = StatePCdata['State Name'];
-        var selState = allStates.filter(selectedState => selectedState == StatePCdata['State Name']);
-        // var StateIndex = allStates.indexOf(selState[0])
+    d3.json(StateProdConsURL).then(function(StatePCdata2) {
+
+        console.log(StatePCdata2)
+        var StatePCdata = StatePCdata2[0];
+
+
+
         var CO2Emissions = StatePCdata['Carbon Dioxide Emission']
         var CO2EmissionsRank = StatePCdata['Carbon Dioxide Emission Rank']
-        var consumptionPerCapita = StatePCdata['Consumption per capita'][StateIndex]
-        var consumptionRank = StatePCdata['Consumption Rank'][StateIndex]
-        var expendituresperCapita = StatePCdata['Expenditure per capita'][StateIndex]
-        var expendituresRank = StatePCdata['Expenditure Rank'][StateIndex]
-        var productionShare = StatePCdata['Production Share'][StateIndex]
-        var productionRank = StatePCdata["Production Rank"][StateIndex]
+        var consumptionPerCapita = StatePCdata['Consumption per capita']
+        var consumptionRank = StatePCdata['Consumption Rank']
+        var expendituresperCapita = StatePCdata['Expenditure per capita']
+        var expendituresRank = StatePCdata['Expenditure Rank']
+        var productionShare = StatePCdata['Production Share']
+        var productionRank = StatePCdata["Production Rank"]
 
-        // console.log(metadata);
-        // filter samples by the state 
-        var result1 = CO2Emissions.filter(metadatum => metadatum.id.toString() === id)[0];
-        console.log(result1);
+        var tableDataArr = [
 
-        var result2 = CO2EmissionsRank.filter(metadatum => metadatum.id.toString() === id)[0];
-        console.log(result2);
-
-        var result3 = consumptionPerCapita.filter(metadatum => metadatum.id.toString() === id)[0];
-        console.log(result3);
-
-        var result4 = consumptionRank.filter(metadatum => metadatum.id.toString() === id)[0];
-        console.log(resul4t);
-
-        var result5 = expendituresperCapita.filter(metadatum => metadatum.id.toString() === id)[0];
-        console.log(result5);
-
-        var result6 = expendituresRank.filter(metadatum => metadatum.id.toString() === id)[0];
-        console.log(result6);
-
-        var result7 = productionShare.filter(metadatum => metadatum.id.toString() === id)[0];
-        console.log(result7);
-
-        var result8 = productionRank.filter(metadatum => metadatum.id.toString() === id)[0];
-        console.log(result8);
+            CO2Emissions, // 1
+            CO2EmissionsRank, // 2
+            consumptionPerCapita, // 3
+            consumptionRank, // 4
+            expendituresperCapita, //5
+            expendituresRank, //6
+            productionShare, //7
+            productionRank
+        ]
 
         // select the demographic table
-        var getSummary = d3.select("#state-summary");
+        var getSummary = d3.select("#state-table");
 
         // clear the demographic table
         getSummary.html("");
 
-        // loop through the info in the metadata and append results to table
-        Object.entries(result).forEach((key) => {
+        var statename = d3.select("#statename")
+        console.log(statename)
+        statename.append("h5").text(`State : ${StatePCdata["State Name"]}`)
+            .append("p")
+
+        //statename.attribute()
+        // loop through the info in the StatePCdata and append results to table
+        Object.entries(StatePCdata).slice(0, -2).forEach((key) => {
             console.log(key)
             getSummary.append("h6").text(key[0] + ": " + key[1] + "\n");
         });
     });
 }
+//Function to create doughnut chart with new library chart.js
+function buildCharts() {
+    console.log("hello2");
+    d3.json(StateEnergySourceURL).then(function(stateEData2) {
+        console.log(stateEData2[0]);
 
-function buildCharts(State) {
+        var stateEData = stateEData2[0];
+        // var pielabels = Object.entries(stateEData).map(key =>
+        //     key)
 
-    d3.json(StateEnergySourceURL, stateEData => {
-        var allStates = stateEData.State;
-        var selState = allStates.filter(selectedState => selectedState == State);
-        var StateIndex = allStates.indexOf(selState[0])
-
-        var coal = stateEData.biopower_gaseous[StateIndex]; //["Fossil Fuel - Coal"]
-        var naturalGas = stateEData.biopower_solid[StateIndex]; //["Fossil Fuel - Natural Gas"]
-        var crudeOil = stateEData.csp_solar[StateIndex]; //['Fossil Fuel - Crude Oil']
-        var nuclear = stateEData.egs_geothermal[StateIndex]; //['Nuclear Power']
-        var bioFuels = stateEData.geotermal_hydrothermal[StateIndex]; //['BioFuels']
-        var woodWaste = stateEData.hydropower[StateIndex]; //['Wood and Waste']
-        var other = stateEData.offshore_wind[StateIndex]; //['Other']
+        ;
+        var coal = stateEData["Fossil Fuel - Coal"]; //["Fossil Fuel - Coal"]
+        console.log(stateEData);
+        var naturalGas = stateEData["Fossil Fuel - Natural Gas"]; //["Fossil Fuel - Natural Gas"]
+        var crudeOil = stateEData['Fossil Fuel - Crude Oil']; //['Fossil Fuel - Crude Oil']
+        var nuclear = stateEData['Nuclear Power']; //['Nuclear Power']
+        var bioFuels = stateEData['BioFuels']; //['BioFuels']
+        var woodWaste = stateEData['Wood and Waste']; //['Wood and Waste']
+        var other = stateEData['Other']; //['Other']
 
 
         var pieDataArr = [
 
-            coal, // 1
-            naturalGas, // 2
-            crudeOil, // 3
-            nuclear, // 4
-            bioFuels, //5
-            woodWaste, //6
-            other //7
+                coal, // 1
+                naturalGas, // 2
+                crudeOil, // 3
+                nuclear, // 4
+                bioFuels, //5
+                woodWaste, //6
+                other //7
 
+            ]
+            //var values = Object.entries(stateEData).map((key, value) => value)
 
-        ]
-
-        var ctx = document.getElementById('pieChart').getContext('2d');
+        var ctx = document.getElementById('pieChart')
         var myPieChart = new Chart(ctx, {
             type: 'doughnut',
             data: {
@@ -106,15 +111,13 @@ function buildCharts(State) {
 
                     ]
                 }],
-                labels: [
-                    "Fossil Fuel - Coal",
+                labels: ["Fossil Fuel - Coal",
                     "Fossil Fuel - Natural Gas",
                     "Fossil Fuel - Crude Oil",
                     "Nuclear Power",
                     "BioFuels",
                     "Wood and Waste",
-                    "Other",
-
+                    "Other"
                 ]
             },
             options: {
@@ -137,45 +140,14 @@ function buildCharts(State) {
 };
 
 
-// Populate the State DropDown & create initial plots 
+//initializing all the functions
 function init() {
-    // Grab a reference to the DropDown
-    var selector = d3.select("#selDataset");
-    // Grab the State Names and populate the dropdown
-    d3.json(energyComparisonURL, StatePCdata => {
-        var States = StatePCdata.State.sort(d3.ascending)
-
-        States.forEach(State => {
-            selector
-                .append("option")
-                .text(State)
-                .property("value", State);
-        });
-        // Create default chart
-        var defaultState = States[0];
-        buildCharts(defaultState);
-
-    });
+    console.log("hello3");
+    buildCharts();
+    stateSummary();
+    createData();
+    createData1();
 };
 
-function StateChange(newState) {
-    // Remove previous chart by removing canvas element
-    d3.select("#pieChart").remove();
-    // Add canvas elements back
-    d3.select("#plot1")
-        .append('canvas')
-        .attr('id', 'pieChart')
-        .attr('width', '400')
-        .attr('height', '400');
-
-    d3.select("state-table")
-        .append('canvas')
-        .attr('id', 'summary-table')
-        .attr('width', '400')
-        .attr('height', '400');
-
-    // Fetch new data each time a new State is selected 
-    buildCharts(newState);
-}
 
 init();
